e9936da93fd72e84adbf652f3db2aed76ae5df42
Nivedita Pathak <110668073+niveditapa21@users.noreply.github.com>
2023-07-28 15:00:35 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world this is India the super power!'
 
 if __name__ == '__main__':
     app.run()
